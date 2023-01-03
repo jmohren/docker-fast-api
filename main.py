fe9e65b80fa28bb849af2173e6c5d4fff0c15a49
john8991 <johannes.mohren@rwth-aachen.de>
2023-01-03 16:04:19 +0100

    python:3.7-slim

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-# from fastapi import FastAPI
-# import uvicorn
-
-# app = FastAPI()
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "world"}
-
-# if __name__ == '__main__':
-#     uvicorn.run(app, port=8000, host="0.0.0.0")
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
